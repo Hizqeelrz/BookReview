e7d42993b49be2d5133903d7dfa263bb0502cf05
Hizqeel <hizqeel1214@gmail.com>
2017-08-05 11:44:21 +0500

    controller action updated with view files created

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,50 @@
 class BooksController < ApplicationController
 
 	def index
-		
+		@books = Book.all.order(created_at: :desc)
 	end
 
 	def new
-		
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+
+		if @book.save
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	def show
+		@book = Book.find(params[:id])
+	end
+
+	def edit
+		@book = Book.find(params[:id])		
+	end
+
+	def update
+		@book = Book.find(params[:id])		
+
+		if @book.update(book_params)
+			redirect_to book_path(book)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@book = Book.find(params[:id])		
+		@book.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def book_params
+		params.require(:book).permit(:title, :author, :description)
 	end
 end
