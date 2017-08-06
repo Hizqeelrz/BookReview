6487d9ae8e3c9a156dd30f2cc2b785aabd0ca3c3
Hizqeel <hizqeel1214@gmail.com>
2017-08-06 17:24:49 +0500

    book_category Controller actions & views files created

--- app/controllers/book_categories_controller.rb
+++ app/controllers/book_categories_controller.rb
@@ -1,35 +1,50 @@
 class BookCategoriesController < ApplicationController
 
 	def index
+		@book_categories = BookCategory.all
 	end
 
 	def new
-		
+		@book_category = BookCategory.new
 	end
 
 	def create
-		
+		@book_category = BookCategory.new(category_params)
+
+		if @book_category.save
+			redirect_to root_path
+		else
+			render :new
+		end
 	end
 
 	def show
-		
+		@book_category = BookCategory.find(params[:id])
 	end
 
 	def edit
-		
+		@book_category = BookCategory.find(params[:id])
 	end
 
 	def update
+		@book_category = BookCategory.find(params[:id])
 		
+		if @book_category.update(category_params)
+			redirect_to book_category_path(@book_category)
+		else
+			render :edit
+		end
 	end
 
 	def destroy
-		
+		@book_category = BookCategory.find(params[:id])
+		@book_category.destroy
+		redirect_to root_path		
 	end
 
 	private
 
 	def category_params
-		
+		params.require(:book_category).permit(:name)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :book_categories
   resources :books
   root "books#index"
 
