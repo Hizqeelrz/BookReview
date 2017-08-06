c653afa48e5f430bc05f8d3795edfaf4f19ee859
Hizqeel <hizqeel1214@gmail.com>
2017-08-06 16:46:02 +0500

    book category created

---
+++ app/controllers/book_categories_controller.rb
@@ -0,0 +1,35 @@
+class BookCategoriesController < ApplicationController
+
+	def index
+	end
+
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	private
+
+	def category_params
+		
+	end
+end
---
+++ test/controllers/book_categories_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BookCategoriesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
